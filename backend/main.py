c1159e0049aaae184ba568d897e23106adfe4b09
Jedidiah Ephraim <jedidiahephraim4@gmail.com>
2026-05-14 21:11:37 -0500

    included budget

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from datetime import datetime
 
 app = FastAPI()
 
 transactions = []
+budgets = {}
 
 class Transaction(BaseModel):
     amount:float
     category: str
     type : str
+
+class Budget(BaseModel):
+    category : str
+    limit : float
+
 
 
 @app.get("/")
@@ -21,28 +28,55 @@ def get_transactions():
 
 @app.post("/transactions")
 def add_transaction(transaction: Transaction):
-    transactions.append(transaction)
+    transaction_data = transaction.dict()
+    transaction_data["date"] = datetime.now().strftime("%Y-%m-%d")
+
+    transactions.append(transaction_data)
+
     return {
         "message": "Transaction added",
-        "transaction":transaction
+        "transaction":transaction_data
     } 
+
+
+@app.post("/budgets")
+def add_budget(budget: Budget):
+    budgets[budget.category] = budget.limit
+
+    return {
+        "message": "Budget set",
+        "budget" : budget
+    }
+
+
 @app.get("/summary")
 def get_summary():
     total_income = 0
     total_expenses = 0
+    total_savings = 0
+    last_updated = None
 
     for transaction in transactions:
-        if transaction.type == "income":
-            total_income += transaction.amount
-        elif transaction.type == "expense":
-            total_expenses += transaction.amount
-    remaining_balance = total_income - total_expenses
+        last_updated = transaction["date"]
 
-    return{
+        if transaction["type"] == "income":
+            total_income += transaction["amount"]
+
+        elif transaction["type"] == "expense":
+            total_expenses += transaction["amount"]
+
+        elif transaction["type"] == "savings":
+            total_savings += transaction["amount"]
+    remaining_balance = total_income - total_expenses - total_savings
+
+    return {
         "total_income" : total_income,
         "total_expenses" : total_expenses,
-        "remaining_balance" : remaining_balance
+        "total_savings" : total_savings,
+        "remaining_balance" : remaining_balance,
+        "last_updated" : last_updated
     }
+
 
 @app.get("/spending-by-category")
 def get_spending_by_category():
